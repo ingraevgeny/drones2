4638909a9742ad611d64c66a4c58ce04f566dbd1
Evgeny Khodok <zenonevg@gmail.com>
2024-07-28 17:17:28 +0300

    customers-sequence-generator to avoid key violation

--- backend/src/main/java/com/evg/drones/model/Drone.java
+++ backend/src/main/java/com/evg/drones/model/Drone.java
@@ -5,6 +5,7 @@ import com.evg.drones.dict.State;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 
 @Entity
@@ -14,7 +15,15 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Drone {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GenericGenerator(
+            name = "customers-sequence-generator",
+            type = org.hibernate.id.enhanced.SequenceStyleGenerator.class,
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "customers_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "10"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            })
+    @GeneratedValue(generator = "customers-sequence-generator")
     @Column(nullable = false)
     private Long id;
     @Column(length = 100)
