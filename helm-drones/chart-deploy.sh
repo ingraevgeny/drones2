b0fc6fe0e9143484898edefa1ef17e232e6175b7
Evgeny Khodok <zenonevg@gmail.com>
2024-07-28 15:00:52 +0300

    delivery microservice added to deployment

--- helm-drones/chart-deploy.sh
+++ helm-drones/chart-deploy.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-svc[drones]=8083
-svc[clients]=8081
-svc[approot]=8085
+declare -A svc
+
+svc["drones"]=8083
+svc["clients"]=8081
+svc["approot"]=8085
+svc["delivery"]=8087
+
+for key in "${!svc[@]}"
+do
+    echo "key: $key, value: ${svc[$key]}"
+done
 
 
 for key in "${!svc[@]}"
 do
   service=$key
   port=${svc[$key]}
+
+  echo "service: " $service
+  echo "port: " $port
+
   helm uninstall $service
   kill -9 `ps -ef | grep $service | awk 'NR==1{print $2}'`
 
   echo "install " $service
   helm install $service oci://registry-1.docker.io/dock999evg/$service --version 201-dev
 
-  kubectl wait --for=condition=Ready pod/$service
+  kubectl wait --for=condition=Ready pod/$service --timeout=180s
 
   trap 'kill 0' SIGINT;
   kubectl port-forward $service $port:$port > /dev/null &
 
   echo =================================
-do
+done
